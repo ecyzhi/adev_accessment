94be8135fb2e3b7ce3f737f453f7638fd17b3dec
ecyzhi <bitonana@gmail.com>
2021-06-11 01:13:55 +0800

    Added Stepper

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  Map<String, int> levelMap = {
+    'Level 1': 0,
+    'Level 2': 1,
+    'Level 3': 2,
+    'Level 4': 3,
+    'Level 5': 4,
+  };
+
   List<String> level = [
     'Level 1',
     'Level 2',
@@ -20,7 +28,37 @@ class _HomepageState extends State<Homepage> {
     'Level 5',
   ];
 
-  String selectedValue = '';
+  String selectedValue = 'Level 1';
+
+  int currentStep = 0;
+
+  List<Step> steps = [
+    Step(
+      title: Text('Level 1'),
+      content: Text('Level 1'),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Level 2'),
+      content: Text('Level 2'),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Level 3'),
+      content: Text('Level 3'),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Level 4'),
+      content: Text('Level 4'),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Level 5'),
+      content: Text('Level 5'),
+      isActive: true,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,28 +68,41 @@ class _HomepageState extends State<Homepage> {
         children: [
           Expanded(
             child: Container(
+              color: Colors.blue,
               padding: EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  DropdownButtonFormField<String>(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      )
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: DropdownButton<String>(
+                      items: level.map((String value) => DropdownMenuItem(value: value, child: Text(value))).toList(),
+                      onChanged: (value){
+                        setState(() {
+                          selectedValue = value;
+                          currentStep = levelMap[selectedValue];
+                        });
+                      },
+                      value: selectedValue,
+                      icon: Icon(Icons.expand_more),
+                      iconEnabledColor: Colors.blue,
+                      iconSize: 40,
+                      isExpanded: true,
                     ),
-                    items: level.map((String value) => DropdownMenuItem(value: value, child: Text(value))).toList(),
-                    onChanged: (value){
-                      setState(() {
-                        selectedValue = value;
-                      });
-                    },
-                    hint: Text('Select Level'),
                   ),
                   Expanded(
                     child: Card(
+                      color: Colors.blue,
                       child: Center(
-                        child: Text(selectedValue),
+                        child: Text(
+                          selectedValue,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -61,7 +112,19 @@ class _HomepageState extends State<Homepage> {
           ),
           Expanded(
             child: Container(
-              color: Colors.grey,
+              child: Stepper(
+                steps: steps,
+                currentStep: currentStep,
+                type: StepperType.vertical,
+                onStepTapped: (step) {
+                  setState(() {
+                    currentStep = step;
+                  });
+                },
+                controlsBuilder: (context, {onStepContinue, onStepCancel}){
+                  return Container();
+                },
+              ),
             ),
           ),
         ],
@@ -69,3 +132,4 @@ class _HomepageState extends State<Homepage> {
     );
   }
 }
+
